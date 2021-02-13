7b60ac3e01c8eeef2d0f30633215b9d9be20b587
Sukhvsin2 <sukhvsingh2026@gmail.com>
2021-02-13 14:40:07 -0600

    Streming Done

--- src/components/VideoStream.vue
+++ src/components/VideoStream.vue
@@ -1,24 +1,28 @@
 <template>
   <h1>Video Stream</h1>
-  <video id="videoPlayer" width="650" controls muted="muted" autoplay>
+  <video id="videoPlayer" width="650" controls autoplay='true'>
       <source src="http://localhost:2828/video" type="video/mp4" />
   </video>
 </template>
 
 <script>
+import axios from "axios"
 export default {
     name: 'VideoStream',
-    mounted(){
+    data(){
+        return{
+            urlUse : ''
+        }
+    },
+    created: function(){
         this.apiCall();
     },
     methods:{
         apiCall: async () => {
-            const headers = {
-                "Content-Range": 'bytes 0'
-            }
             try {
-                var res = await fetch('http://localhost:2828/video', headers)
-                console.log(await res.json());
+                let res = await axios.get('http://localhost:2828/video')
+                console.log('check res ', res);
+                this.urlUse = res.data;
             } catch (error) {
                 console.log(error);
             }
